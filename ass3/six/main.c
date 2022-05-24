689f3b13463f79d374d421f4dcebf1ecc3c66f76
Moataz-Hamed <64382314+Moataz-Hamed@users.noreply.github.com>
2022-05-24 16:25:29 +0200

    Add files via upload

---
+++ ass3/six/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(){
+
+	char st[100],c;
+	int a=0,i;
+	printf("Enter a String: ");
+	fflush(stdout);fflush(stdin);
+	gets(st);
+	printf("Enter the character: ");
+	fflush(stdout);fflush(stdin);
+	scanf("%c",&c);
+	for(i=0;i<strlen(st);i++){
+		if(c==tolower(st[i])){
+			a=a+1;
+			continue;
+		}
+	}
+	printf("%d",a);
+}
