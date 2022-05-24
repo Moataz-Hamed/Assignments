8a30838e474beef658b457eef80d760805feae03
Moataz-Hamed <64382314+Moataz-Hamed@users.noreply.github.com>
2022-05-24 16:07:23 +0200

    Add files via upload

---
+++ ass1/HW1/six/main.c
@@ -0,0 +1,26 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 26, 2022
+ *      Author: Moataz
+ */
+#include "stdio.h"
+
+int main() {
+
+	float a,b,c;
+	printf("Enter value of a: \r\nEnter value of b:");
+	printf(" ");
+	fflush(stdin); fflush(stdout);
+	scanf("%f %f",&a,&b);
+	c=a;
+	a=b;
+	b=c;
+	printf("After swapping, value of a = %f",a);
+	printf("After swapping, value of b = %f",b);
+
+
+
+	return 0;
+}
+
