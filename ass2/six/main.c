4468dfb9de7d889de1e40cfa3e54396b6cee6b93
Moataz-Hamed <64382314+Moataz-Hamed@users.noreply.github.com>
2022-05-24 16:14:29 +0200

    Add files via upload

---
+++ ass2/six/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+
+	int a,res=0;
+	printf("Enter the Number: ");
+	fflush(stdout);fflush(stdin);
+	scanf("%d",&a);
+	for(int i=a;i!=0;i--){
+		res+=i;
+	}
+	printf("%d",res);
+}
