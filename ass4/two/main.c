bc0453dcf773f0044a4dda9a99826824ef3dfeb1
Moataz-Hamed <64382314+Moataz-Hamed@users.noreply.github.com>
2022-05-24 16:31:07 +0200

    Add files via upload

---
+++ ass4/two/main.c
@@ -0,0 +1,29 @@
+/*
+ * main.c
+ *
+ *  Created on: May 15, 2022
+ *      Author: Moataz
+ */
+
+
+// Calculate the factorial of a number
+
+#include <stdio.h>
+
+int recursive(int num);
+int main() {
+
+	int a;
+	printf("Enter the number \n");
+	fflush(stdout); fflush(stdin);
+	scanf("%d",&a);
+	int result;
+	result = recursive(a);
+	printf("%d",result);
+	return 0;
+}
+
+int recursive(int num){
+	if(num!=1)
+		return num*recursive(num-1);
+}
