9a8ad7a6b07933471ff6b472af26b52507e9d547
Moataz-Hamed <64382314+Moataz-Hamed@users.noreply.github.com>
2022-05-24 16:33:17 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,29 @@
 
+//factorial using recursion
+
 #include <stdio.h>
 
-void main(){
+void rec(char arr2[],int a);
 
-	char st[100];
-	int i;
-	printf("Enter a String: ");
+int main () {
+
+	char arr[50];
+	int a;
+	printf("Enter a sentence: ");
 	fflush(stdout);fflush(stdin);
-	gets(st);
-	for(i=strlen(st)-1;i>=0;i--){
-		printf("%c",st[i]);
-		}
+	gets(arr);
+	a=strlen(arr);
+	for (int i=0;i<a;i++){
+		printf("%c",arr[i]);
+	}
+	printf("\nReverse is: ");
+	rec(arr,a);
+	return 0;
+}
 
-
+void rec(char arr2[],int a){
+	if(a>=0){
+	printf("%c",arr2[a]);
+	rec(arr2,--a);
+}
 }
