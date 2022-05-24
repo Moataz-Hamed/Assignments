5c56357e8917ed6ada09ae4ecd37fd66b95828c8
Moataz-Hamed <64382314+Moataz-Hamed@users.noreply.github.com>
2022-05-24 16:27:15 +0200

    Add files via upload

---
+++ ass3/eight/main.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+void main(){
+
+	char st[100];
+	int i;
+	printf("Enter a String: ");
+	fflush(stdout);fflush(stdin);
+	gets(st);
+	for(i=strlen(st)-1;i>=0;i--){
+		printf("%c",st[i]);
+		}
+
+
+}
