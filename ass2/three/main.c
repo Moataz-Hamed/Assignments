66df6af7a840223f26386227ad6ae1f5ac5b975b
Moataz-Hamed <64382314+Moataz-Hamed@users.noreply.github.com>
2022-05-24 16:11:48 +0200

    Add files via upload

---
+++ ass2/three/main.c
@@ -0,0 +1,30 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 27, 2022
+ *      Author: Moataz
+ */
+
+
+#include"stdio.h"
+
+void main (){
+		float a,b,c;
+		printf("Enter three numbers");
+		fflush(stdout);fflush(stdin);
+		scanf("%f %f %f",&a ,&b,&c);
+		if(a>b){
+			if(a>c){
+				printf("%f is the biggest",a);
+			}else if(a<c){
+				printf("%f is the biggest",c);
+			}
+		}else{
+			if(b>c){
+				printf("%f is the biggest",b);
+			}else if(b<c){
+				printf("%f is the biggest",c);
+			}
+		}
+
+}
