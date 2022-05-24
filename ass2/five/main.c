e77656442ca1d23606b739c0f92c17a85a540930
Moataz-Hamed <64382314+Moataz-Hamed@users.noreply.github.com>
2022-05-24 16:13:33 +0200

    Add files via upload

---
+++ ass2/five/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	char a;
+	int b;
+	printf("Enter a character");
+	fflush(stdout);fflush(stdin);
+	scanf("%c",&a);
+	b=a;
+	if(65<=b && b<=90 || 97<=b && 	b<=122){
+		printf("%c is a character",a);
+	}else{
+		printf("%c not a character",a);
+	}
+
+	return 0;
+}
