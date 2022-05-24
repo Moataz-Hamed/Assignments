6d66d6bfc7825755ca6c2f4cb5b5099e35cca3de
Moataz-Hamed <64382314+Moataz-Hamed@users.noreply.github.com>
2022-05-24 16:15:40 +0200

    Add files via upload

---
+++ ass2/eight/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void main() {
+
+	float a,b;
+	char c;
+	float res;
+	printf("Enter operator: ");
+	fflush(stdout);fflush(stdin);
+	scanf("%c",&c);
+	printf("Enter two numbers");
+	fflush(stdout);fflush(stdin);
+	scanf("%f %f",&a,&b);
+	switch (c){
+		case'-':
+			res=a-b;
+			break;
+		case'+':
+			res=a+b;
+			break;
+		case'*':
+			res=a*b;
+			break;
+		case'/':
+			res=a/b;
+			break;
+		default:
+			printf("not a valid operator");
+			break;
+	}
+
+	printf("%f",res);
+}
