76930e6d622dab86b916fb574fbf07f1dcfdf8d7
Moataz-Hamed <64382314+Moataz-Hamed@users.noreply.github.com>
2022-05-24 16:15:05 +0200

    Add files via upload

---
+++ ass2/seven/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() {
+
+	int a,res=1;
+	printf("Enter the Number: ");
+	fflush(stdout);fflush(stdin);
+	scanf("%d",&a);
+	if(a<0){
+		printf("Factorial of a negative number does not exist");
+	}else{
+	for(int i=a;i!=0;i--){
+		res*=i;
+	}
+	}
+	printf("%d",res);
+}
